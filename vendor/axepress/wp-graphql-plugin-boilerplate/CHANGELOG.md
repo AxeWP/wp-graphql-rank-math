8f843f3aa9d044714b9ed8b3ef3912bbe05461a0
David Levine <justlevine@gmail.com>
2022-08-06 18:12:38 +0000

    chore: update to wgpb v0.0.3

--- vendor/axepress/wp-graphql-plugin-boilerplate/CHANGELOG.md
+++ vendor/axepress/wp-graphql-plugin-boilerplate/CHANGELOG.md
@@ -1,3 +1,14 @@
 # Changelog
 
-@todo
+## v0.0.3
+* chore: Update composer deps.
+* chore: Exclude `wp-graphql-plugin-name` when installing as composer dep.
+
+## v0.0.2
+* Dev: Build composer deps on PHP 7.4.
+* Chore: Update composer deps.
+* Chore: Remove unused PHPStan ignored error.
+* CI: Update PHP version used for CodeQuality.
+
+## v0.0.1
+* Initial Release
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,13 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
-    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
     'WPGraphQL\\RankMath\\' => array($baseDir . '/src'),
-    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src/Prophecy'),
-    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
-    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
-    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
-    'Codeception\\PHPUnit\\' => array($vendorDir . '/codeception/phpunit-wrapper/src'),
     'AxeWP\\GraphQL\\' => array($vendorDir . '/axepress/wp-graphql-plugin-boilerplate/src'),
 );
