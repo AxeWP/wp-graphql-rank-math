014a34a6c1150792de168d39a12d7e479ddbd1b8
David Levine <david@axepress.dev>
2022-09-04 12:57:31 +0000

    test: add sitemap setting tests

--- tests/wpunit/SettingsQueriesTest.php
+++ tests/wpunit/SettingsQueriesTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use RankMath\Helper;
+
 /**
  * Tests Settings queries.
  */
@@ -31,7 +33,10 @@ class SettingsQueriesTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCase {
 		update_option( 'show_on_front', 'page' );
 		update_option( 'page_for_posts', $page_id );
 
-		WPGraphQL::clear_schema();
+		Helper::update_modules( [ 'sitemap' => 'on' ] );
+
+
+		$this->clearSchema();
 	}
 
 	/**
@@ -426,6 +431,113 @@ class SettingsQueriesTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCase {
 											]
 										),
 									]
+								),
+							]
+						),
+					]
+				),
+			]
+		);
+	}
+
+	/**
+	 * Tests rankMathSettings.sitemap
+	 */
+	public function testSitemapSettings() {
+		// Configure settings.
+		$title_options = get_option( 'rank-math-options-titles', [] );
+
+		$title_options['disable_author_archives'] = 'off';
+		rank_math()->settings->set( 'titles', 'disable_author_archives', false );
+
+		$title_options['author_custom_robots'] = 'off';
+		rank_math()->settings->set( 'titles', 'author_custom_robots', false );
+
+		update_option( 'rank-math-options-titles', $title_options );
+
+		$query = '{
+			rankMathSettings {
+				sitemap {
+					author {
+						excludedRoles
+						excludedUserDatabaseIds
+						sitemapUrl
+					}
+					contentTypes {
+						customImageMetaKeys
+						isInSitemap
+						sitemapUrl
+						type
+					}
+					general {
+						canPingSearchEngines
+						excludedPostDatabaseIds
+						excludedTermDatabaseIds
+						hasFeaturedImage
+						hasImages
+						linksPerSitemap
+					}
+					indexUrl
+					taxonomies {
+						hasEmptyTerms
+						isInSitemap
+						sitemapUrl
+						type
+					}
+				}
+			}
+		}';
+
+		$actual = $this->graphql( compact( 'query' ) );
+
+		$this->assertQuerySuccessful(
+			$actual,
+			[
+				$this->expectedObject(
+					'rankMathSettings',
+					[
+						$this->expectedObject(
+							'sitemap',
+							[
+								$this->expectedObject(
+									'author',
+									[
+										$this->expectedField( 'excludedRoles', [ 'CONTRIBUTOR', 'SUBSCRIBER' ] ),
+										$this->expectedField( 'excludedUserDatabaseIds', static::IS_NULL ),
+										$this->expectedField( 'sitemapUrl', get_home_url() . '/author-sitemap.xml' ),
+									]
+								),
+								$this->expectedNode(
+									'contentTypes',
+									[
+										$this->expectedField( 'customImageMetaKeys', static::IS_NULL ),
+										$this->expectedField( 'isInSitemap', true ),
+										$this->expectedField( 'sitemapUrl', get_home_url() . '/post-sitemap.xml' ),
+										$this->expectedField( 'type', 'POST' ),
+									],
+									0
+								),
+								$this->expectedObject(
+									'general',
+									[
+										$this->expectedField( 'canPingSearchEngines', true ),
+										$this->expectedField( 'excludedPostDatabaseIds', static::IS_NULL ),
+										$this->expectedField( 'excludedTermDatabaseIds', static::IS_NULL ),
+										$this->expectedField( 'hasFeaturedImage', false ),
+										$this->expectedField( 'hasImages', true ),
+										$this->expectedField( 'linksPerSitemap', 200 ),
+									]
+								),
+								$this->expectedField( 'indexUrl', get_home_url() . '/sitemap_index.xml' ),
+								$this->expectedNode(
+									'taxonomies',
+									[
+										$this->expectedField( 'hasEmptyTerms', false ),
+										$this->expectedField( 'isInSitemap', true ),
+										$this->expectedField( 'sitemapUrl', get_home_url() . '/category-sitemap.xml' ),
+										$this->expectedField( 'type', 'CATEGORY' ),
+									],
+									0
 								),
 							]
 						),
