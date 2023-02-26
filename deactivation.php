3d4f6d1c3fb15b8f1cb298a3beb545c6bc42dcfe
David Levine <david@axepress.dev>
2023-02-26 16:57:40 +0000

    dev: wrap global functions in `function_exists()`

--- deactivation.php
+++ deactivation.php
@@ -5,51 +5,55 @@
  * @package WPGraphql\RankMath
  */
 
-/**
- * Runs when WPGraphQL is de-activated.
- *
- * This cleans up data that WPGraphQL stores.
- */
-function graphql_seo_deactivation_callback() : callable {
-	return function() : void {
+if ( ! function_exists( 'graphql_seo_deactivation_callback' ) ) {
+	/**
+	 * Runs when WPGraphQL is de-activated.
+	 *
+	 * This cleans up data that WPGraphQL stores.
+	 */
+	function graphql_seo_deactivation_callback() : callable {
+		return function() : void {
 
-		// Fire an action when WPGraphQL is de-activating.
-		do_action( 'graphql_seo_deactivate' );
+			// Fire an action when WPGraphQL is de-activating.
+			do_action( 'graphql_seo_deactivate' );
 
-		// Delete data during activation.
-		graphql_seo_delete_data();
-	};
+			// Delete data during activation.
+			graphql_seo_delete_data();
+		};
+	}
 }
 
-/**
- * Delete data on deactivation.
- */
-function graphql_seo_delete_data() : void {
+if ( ! function_exists( 'graphql_seo_delete_data' ) ) {
+	/**
+	 * Delete data on deactivation.
+	 */
+	function graphql_seo_delete_data() : void {
 
-	// Check if the plugin is set to delete data or not.
-	$delete_data = graphql_seo_get_setting( 'delete_data_on_deactivate' );
+		// Check if the plugin is set to delete data or not.
+		$delete_data = graphql_seo_get_setting( 'delete_data_on_deactivate' );
 
-	// Bail if not set to delete.
-	if ( 'on' !== $delete_data ) {
-		return;
-	}
-
-	// Delete plugin version.
-	delete_option( 'wp_graphql_seo_version' );
-
-	// Initialize the settings API.
-	$settings = new \WPGraphQL\RankMath\Admin\Settings\Settings();
-	$settings::register_settings();
-
-	// Get all the registered settings fields.
-	$fields = $settings::get_settings_api()->get_settings_fields();
-
-	// Loop over the registered settings fields and delete the options.
-	if ( ! empty( $fields ) && is_array( $fields ) ) {
-		foreach ( $fields as $group => $fields ) {
-			delete_option( $group );
+		// Bail if not set to delete.
+		if ( 'on' !== $delete_data ) {
+			return;
 		}
-	}
 
-	do_action( 'graphql_seo_delete_data' );
+		// Delete plugin version.
+		delete_option( 'wp_graphql_seo_version' );
+
+		// Initialize the settings API.
+		$settings = new \WPGraphQL\RankMath\Admin\Settings\Settings();
+		$settings::register_settings();
+
+		// Get all the registered settings fields.
+		$fields = $settings::get_settings_api()->get_settings_fields();
+
+		// Loop over the registered settings fields and delete the options.
+		if ( ! empty( $fields ) && is_array( $fields ) ) {
+			foreach ( $fields as $group => $fields ) {
+				delete_option( $group );
+			}
+		}
+
+		do_action( 'graphql_seo_delete_data' );
+	}
 }
