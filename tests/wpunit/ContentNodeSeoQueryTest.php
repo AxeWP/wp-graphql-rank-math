33a3c699a3df7e7a5dd5a2efa9572e7d1c9dead1
David Levine <david@axepress.dev>
2023-05-10 18:28:45 +0000

    tests: test against draft revision

--- tests/wpunit/ContentNodeSeoQueryTest.php
+++ tests/wpunit/ContentNodeSeoQueryTest.php
@@ -92,7 +92,6 @@ class ContentNodeSeoQueryTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCas
 				$this->expectedObject(
 					'contentNode',
 					[
-						$this->expectedField( 'databaseId', $this->database_id ),
 						$this->expectedObject(
 							'seo',
 							[
@@ -152,6 +151,8 @@ class ContentNodeSeoQueryTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCas
 		$actual = $this->graphql( compact( 'query', 'variables' ) );
 		$this->assertArrayNotHasKey( 'errors', $actual );
 
+		$this->assertEquals( $this->database_id, $actual['data']['contentNode']['databaseId'] );
+
 		$this->assertValidSeo( $actual );
 
 		// Test individual values:
@@ -163,10 +164,22 @@ class ContentNodeSeoQueryTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCas
 	 * Tests seo on the contentNode preview.
 	 */
 	public function testContentNodeSeoAsPreview() {
+		// Test revision on existing post.
+		wp_set_current_user( $this->admin );
+		$revision_id = $this->factory()->post->create(
+			[
+				'post_type'   => 'revision',
+				'post_status' => 'inherit',
+				'post_title'  => 'Post Title',
+				'post_author' => $this->admin,
+				'post_parent' => $this->database_id,
+			]
+		);
+
 		$query = $this->get_query();
 
 		$variables = [
-			'id'        => $this->database_id,
+			'id'        => $revision_id,
 			'idType'    => 'DATABASE_ID',
 			'asPreview' => true,
 		];
@@ -174,10 +187,86 @@ class ContentNodeSeoQueryTest extends \Tests\WPGraphQL\TestCase\WPGraphQLTestCas
 		$actual = $this->graphql( compact( 'query', 'variables' ) );
 		$this->assertArrayNotHasKey( 'errors', $actual );
 
+		$this->assertEquals( $revision_id, $actual['data']['contentNode']['databaseId'] );
+
 		$this->assertValidSeo( $actual );
 
-		// Test individual values:
-		$this->assertStringContainsString( home_url(), $actual['data']['contentNode']['seo']['canonicalUrl'] );
-		$this->assertStringContainsString( '<script type="application/ld+json" class="rank-math-schema">', $actual['data']['contentNode']['seo']['jsonLd']['raw'] );
+		// Test revision on draft.
+		$draft_id = $this->factory()->post->create(
+			[
+				'post_type'   => $this->with_post_type,
+				'post_status' => 'draft',
+				'post_title'  => 'Draft Title',
+				'post_author' => $this->admin,
+			]
+		);
+
+		$revision_id = $this->factory()->post->create(
+			[
+				'post_type'   => 'revision',
+				'post_status' => 'inherit',
+				'post_title'  => 'Draft Title',
+				'post_author' => $this->admin,
+				'post_parent' => $draft_id,
+			]
+		);
+
+		$variables = [
+			'id'        => $revision_id,
+			'idType'    => 'DATABASE_ID',
+			'asPreview' => true,
+		];
+
+		$actual = $this->graphql( compact( 'query', 'variables' ) );
+		$this->assertArrayNotHasKey( 'errors', $actual );
+
+		$this->assertQuerySuccessful(
+			$actual,
+			[
+				$this->expectedObject(
+					'contentNode',
+					[
+						$this->expectedObject(
+							'seo',
+							[
+								$this->expectedNode(
+									'breadcrumbs',
+									[
+										$this->expectedField( 'text', 'Draft Title' ),
+										$this->expectedField( 'url', get_permalink( $draft_id ) ),
+										$this->expectedField( 'isHidden', false ),
+									],
+									2
+								),
+								$this->expectedField( 'breadcrumbTitle', 'Draft Title' ),
+								$this->expectedField( 'description', get_the_excerpt( $draft_id ) ),
+								$this->expectedField( 'focusKeywords', static::IS_NULL ),
+								$this->expectedField( 'isPillarContent', false ),
+								$this->expectedField(
+									'robots',
+									[
+										'index',
+										'follow',
+										'max-snippet:-1',
+										'max-video-preview:-1',
+										'max-image-preview:large',
+									]
+								),
+								$this->expectedField( 'title', 'Draft Title - Test' ),
+								$this->expectedObject(
+									'seoScore',
+									[
+										$this->expectedField( 'badgeHtml', static::IS_NULL ),
+										$this->expectedField( 'hasFrontendScore', false ),
+										$this->expectedField( 'rating', 'UNKNOWN' ),
+										$this->expectedField( 'score', 0 ),
+									]
+								),
+							]
+						),
+					]
+				),
+			]
+		);
 	}
 }
