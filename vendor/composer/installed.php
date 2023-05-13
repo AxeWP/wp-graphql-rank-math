06defb8e97b0ca8409b909e67bd21b6d9576177b
David Levine <david@axepress.dev>
2023-05-13 12:43:00 +0000

    chore: version bump

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'axepress/wp-graphql-rank-math',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => 'ea39df8a0e3833146dc7801868c3216872b60b23',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '81b79a0dfde8c749bfc7cb3b05db37a4dc88aebe',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'axepress/wp-graphql-rank-math' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => 'ea39df8a0e3833146dc7801868c3216872b60b23',
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '81b79a0dfde8c749bfc7cb3b05db37a4dc88aebe',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- wp-graphql-rank-math.php
+++ wp-graphql-rank-math.php
@@ -7,7 +7,7 @@
  * Author: AxePress
  * Author URI: https://github.com/AxeWP
  * Update URI: https://github.com/AxeWP/wp-graphql-rank-math
- * Version: 0.0.11
+ * Version: 0.0.12
  * Text Domain: wp-graphql-rank-math
  * Domain Path: /languages
  * Requires at least: 5.4.1
@@ -20,7 +20,7 @@
  * @package WPGraphQL\RankMath
  * @author axepress
  * @license GPL-3
- * @version 0.0.11
+ * @version 0.0.12
  */
 
 // Exit if accessed directly.
@@ -53,7 +53,7 @@ if ( ! function_exists( 'graphql_seo_constants' ) ) {
 	function graphql_seo_constants() : void {
 		// Plugin version.
 		if ( ! defined( 'WPGRAPHQL_SEO_VERSION' ) ) {
-			define( 'WPGRAPHQL_SEO_VERSION', '0.0.11' );
+			define( 'WPGRAPHQL_SEO_VERSION', '0.0.12' );
 		}
 
 		// Plugin Folder Path.
@@ -107,7 +107,7 @@ if ( ! function_exists( 'graphql_seo_plugin_conflicts' ) ) {
 	/**
 	 * Checks if any known plugin conflicts are present.
 	 *
-	 * @since @todo
+	 * @since 0.0.12
 	 */
 	function graphql_seo_plugin_conflicts() : array {
 		$conflicts = [];
