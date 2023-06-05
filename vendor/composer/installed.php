294f0cbd396a0e1daf77b0a25322402c3f3292df
David Levine <david@axepress.dev>
2023-06-05 11:22:20 +0000

    chore: update deps to final versions

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'axepress/wp-graphql-rank-math',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ff6b2ce9e9c0b29202191ad4f5969526a37de06b',
+        'reference' => '70a74064f4598c09090de2f6e7410977b928a72a',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'axepress/wp-graphql-plugin-boilerplate' => array(
-            'pretty_version' => 'dev-dev/remove-type_registry-property',
-            'version' => 'dev-dev/remove-type_registry-property',
-            'reference' => '689d10c9f20c046e255fc11ca53fc201e6fe47b7',
+            'pretty_version' => '0.0.9',
+            'version' => '0.0.9.0',
+            'reference' => '5062b5b8896d14b8ee566b92fffa4d049e7abd34',
             'type' => 'library',
             'install_path' => __DIR__ . '/../axepress/wp-graphql-plugin-boilerplate',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'axepress/wp-graphql-rank-math' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ff6b2ce9e9c0b29202191ad4f5969526a37de06b',
+            'reference' => '70a74064f4598c09090de2f6e7410977b928a72a',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
