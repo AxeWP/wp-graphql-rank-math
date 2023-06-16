4cb8951dcab60a6d302d279570334abf522d641e
David Levine <david@axepress.dev>
2023-06-16 10:41:35 +0000

    chore: version bump

--- wp-graphql-rank-math.php
+++ wp-graphql-rank-math.php
@@ -7,7 +7,7 @@
  * Author: AxePress
  * Author URI: https://github.com/AxeWP
  * Update URI: https://github.com/AxeWP/wp-graphql-rank-math
- * Version: 0.0.13
+ * Version: 0.0.14
  * Text Domain: wp-graphql-rank-math
  * Domain Path: /languages
  * Requires at least: 5.4.1
@@ -20,7 +20,7 @@
  * @package WPGraphQL\RankMath
  * @author axepress
  * @license GPL-3
- * @version 0.0.13
+ * @version 0.0.14
  */
 
 // Exit if accessed directly.
@@ -53,7 +53,7 @@ if ( ! function_exists( 'graphql_seo_constants' ) ) {
 	function graphql_seo_constants(): void {
 		// Plugin version.
 		if ( ! defined( 'WPGRAPHQL_SEO_VERSION' ) ) {
-			define( 'WPGRAPHQL_SEO_VERSION', '0.0.13' );
+			define( 'WPGRAPHQL_SEO_VERSION', '0.0.14' );
 		}
 
 		// Plugin Folder Path.
