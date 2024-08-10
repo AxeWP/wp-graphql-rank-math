f16b6a2acc7f11ed6beb10925b0ebe7f09543e25
David Levine <david@axepress.dev>
2024-08-10 19:08:48 +0000

    tests: stub woographql test as reminder

---
+++ tests/wpunit/WooGraphQLTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Tests WooGraphQL Integration.
+ *
+ * @todo Implement once the ProductVariation.metaData interface conflict is fixed.
+ */
+class WooGraphQLTest extends \Codeception\TestCase\WPTestCase {
+
+	/**
+	 * Stub a test as a reminder
+	 */
+	public function testWooGraphQL() {
+		$this->markTestIncomplete( 'We cannot test WooGraphQL until the ProductVariation.metaData interface conflict is fixed.' );
+	}
+}
