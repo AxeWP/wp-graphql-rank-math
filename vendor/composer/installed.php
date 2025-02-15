573e77f0b4f7c43b85a49e2dfb188742978eb72e
Dovid Levine <david@axepress.dev>
2025-02-15 23:25:10 +0200

    chore: bump PHPStan to v2.0.x (#118)
    * chore: bump PHPStan to v2.0.x
    
    * chore: scan `seo-by-rank-math/includes`
    
    * chore: more PHPStan fixes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'axepress/wp-graphql-rank-math',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '1cf41e140fb2c8b810cdb4dc93f6befec9bf482a',
+        'reference' => '191148867d74f25ed7ae2ffd3fef88dd8c1f77a1',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'axepress/wp-graphql-rank-math' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '1cf41e140fb2c8b810cdb4dc93f6befec9bf482a',
+            'reference' => '191148867d74f25ed7ae2ffd3fef88dd8c1f77a1',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
