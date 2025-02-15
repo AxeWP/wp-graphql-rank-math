e4d8b471abccdf41aaf3c118abe046bd26d68670
Dovid Levine <david@axepress.dev>
2025-02-15 12:42:12 +0200

    chore: test compat w. WP6.7 + WPGraphQL 2.0

--- readme.txt
+++ readme.txt
@@ -2,15 +2,17 @@
 Contributors: axepress, justlevine
 Tags: GraphQL, Gatsby, Headless, WPGraphQL, React, Rest, RankMath, Seo, Schema
 Requires at least: 6.0
-Tested up to: 6.6.1
+Tested up to: 6.7.2
 Requires PHP: 7.4
-Requires Plugins: wp-graphql, seo-by-rank-math
-Requires WPGraphQL: 1.26.0
 Stable tag: 0.3.2
+Maintained at: https://github.com/AxeWP/wp-graphql-headless-login
 License: GPL-3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
 == Description ==
+
+A WordPress plugin that exposes [Rank Math SEO](https://wordpress.org/plugins/seo-by-rank-math/) data to the [WPGraphQL](https://wordpress.org/plugins/wp-graphql/) schema.
+
 == Upgrade Notice ==
 == Frequently Asked Questions ==
 == Screenshots ==
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'axepress/wp-graphql-rank-math',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => 'a1190476f9363bb85f37f84cab566d8caadf4553',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '1cf41e140fb2c8b810cdb4dc93f6befec9bf482a',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'axepress/wp-graphql-rank-math' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => 'a1190476f9363bb85f37f84cab566d8caadf4553',
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '1cf41e140fb2c8b810cdb4dc93f6befec9bf482a',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
