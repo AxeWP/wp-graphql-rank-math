3bd3ede8c79c3bdce32ba2d7c6a6c59837df5fd7
Dovid Levine <david@axepress.dev>
2025-05-03 14:53:37 +0300

    fix: interface conflicts with WooGraphQL v0.21.1+ (#120)
    * Fix WooGraphQL Product Type Expectations
    
    * chore: phpcbf
    
    * dev: add b/c with WooGraphQL < 0.21.1
    
    * fix: add `ProductVariation` to woo product types
    
    * chore: update changelog
    
    * ci: bump woographql testing version
    
    ---------
    
    Co-authored-by: Rob Belfort <rob.belfort@protonmail.com>

--- src/Extensions/WPGraphQLWooCommerce/Type/WPObject/SeoObjects.php
+++ src/Extensions/WPGraphQLWooCommerce/Type/WPObject/SeoObjects.php
@@ -35,11 +35,46 @@ class SeoObjects implements Registrable {
 	public static function register(): void {
 		// Set SEO field types for product children.
 		$product_types = WP_GraphQL_WooCommerce::get_enabled_product_types();
+		/**
+		 * @todo: remove this when we don't need to support WooGraphQL< 0.21.1
+		 * @see https://github.com/AxeWP/wp-graphql-rank-math/pull/115#issuecomment-2660900767
+		 */
+		if ( defined( 'WPGRAPHQL_WOOCOMMERCE_VERSION' ) && version_compare( WPGRAPHQL_WOOCOMMERCE_VERSION, '0.21.1', '>=' ) ) {
+			$product_types = array_merge(
+				$product_types,
+				[
+					'DownloadableProduct',
+					'InventoriedProduct',
+					'ProductUnion',
+					'ProductWithAttributes',
+					'ProductWithDimensions',
+					'ProductWithPricing',
+					'ProductWithVariations',
+					'ProductVariation',
+				]
+			);
+		}
 
 		foreach ( $product_types as $graphql_type_name ) {
 			Utils::overload_graphql_field_type( $graphql_type_name, 'seo', 'RankMathProductObjectSeo' );
 		}
 
+		/**
+		 * @todo: remove this when we don't need to support WooGraphQL< 0.21.1
+		 * @see https://github.com/AxeWP/wp-graphql-rank-math/pull/115#issuecomment-2660900767
+		 */
+		if ( defined( 'WPGRAPHQL_WOOCOMMERCE_VERSION' ) && version_compare( WPGRAPHQL_WOOCOMMERCE_VERSION, '0.21.1', '<' ) ) {
+			self::register_product_variation_types();
+		}
+	}
+
+	/**
+	 * Registers the SEO types for product variations.
+	 *
+	 * @todo: remove this when we don't need to support WooGraphQL< 0.21.1
+	 * @see https://github.com/AxeWP/wp-graphql-rank-math/pull/115#issuecomment-2660900767
+	 */
+	private static function register_product_variation_types(): void {
 		// Register the Product Variation SEO type and apply it to the Product Variation and children.
 		$type_name_for_product_variation = 'RankMathProductVariationObjectSeo';
 
